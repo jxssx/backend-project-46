8636f6ec4427fa57864467f877bc94f8a2a8f1b8
jxssx <danil.druzhina@gmail.com>
2022-11-10 21:42:12 +0300

    change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,13 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .helpOption('-h --help', 'output usage information');
+
+  program.option('-f, --format <type>', 'output format');
+
+
 
 program.parse();

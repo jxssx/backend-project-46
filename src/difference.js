5c65dbcd7c764ff7bc77731b5778d5d64a753dfd
jxssx <danil.druzhina@gmail.com>
2022-11-13 00:01:33 +0300

    change difference.js

--- src/difference.js
+++ src/difference.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import * as fs from 'fs';
 
 const genDiffJSON = (file1, file2) => {
   const data = _.uniq(_.sortBy(_.union(_.keys(file1), _.keys(file2))));
@@ -7,15 +6,15 @@ const genDiffJSON = (file1, file2) => {
     const value1 = file1[key];
     const value2 = file2[key];
     if (_.has(file1, key) && _.has(file2, key)) {
-        if (value1 == value2) {
-            return `  ${key}: ${value1}`;
-        }
-        return [`  -${key}: ${value1}`, `  +${key}: ${value2}`]
+      if (value1 === value2) {
+        return `  ${key}: ${value1}`;
       }
-    if (_.has(file1, key)) {
-        return `  -${key}: ${value1}`;
+      return [`  -${key}: ${value1}`, `  +${key}: ${value2}`];
     }
-    return `  +${key}: ${value2}`
+    if (_.has(file1, key)) {
+      return `  -${key}: ${value1}`;
+    }
+    return `  +${key}: ${value2}`;
   });
   return result.flat();
 };

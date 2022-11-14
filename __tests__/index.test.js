a929c655b9c06317465e932013ec3717ba7b0c73
jxssx <danil.druzhina@gmail.com>
2022-11-14 17:03:22 +0300

    fix index.test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-undef */
 import diff from '../index.js';
-import { readFile, formatPath } from '../src/tools.js';
+import { readFile } from '../src/tools.js';
 
 test('diff', () => {
   const absPath = diff('/home/jxssx/backend-project-46/__fixtures__/file1.json', '/home/jxssx/backend-project-46/__fixtures__/file2.json');
   expect(absPath).toEqual(readFile('/home/jxssx/backend-project-46/__fixtures__/expected.txt'));
+
+  const relPath = diff('__fixtures__/file1.json', '__fixtures__/file2.json');
+  expect(relPath).toEqual(readFile('/home/jxssx/backend-project-46/__fixtures__/expected.txt'));
 });

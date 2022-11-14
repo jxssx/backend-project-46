1ada669104b9f9c6a0d9d70f05912bca1d337891
jxssx <danil.druzhina@gmail.com>
2022-11-14 19:21:19 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,5 +4,5 @@ import { readFile } from '../src/tools.js';
 
 test('diff', () => {
   const actual = diff('__fixtures__/file1.json', '__fixtures__/file2.json');
-  expect(actual).toEqual(readFile(['/home/jxssx/backend-project-46/__fixtures__/expected.txt'], 'utf8'));
+  expect(actual).toEqual(readFile(['__fixtures__/expected.txt'], 'utf8'));
 });

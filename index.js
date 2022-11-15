91c9cc782b3900d7623f2fc88d8a9aa39788193b
jxssx <danil.druzhina@gmail.com>
2022-11-15 13:19:51 +0300

    change index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import { readFile } from './src/tools.js';
+import parse from './src/parsers.js';
+import { readFile, getExt } from './src/tools.js';
 import formatDefault from './src/formatters/default.js';
 import genDiff from './src/difference.js';
 
 const diff = (filepath1, filepath2) => {
-  const file1 = JSON.parse(readFile([filepath1], 'utf8'));
-  const file2 = JSON.parse(readFile([filepath2], 'utf8'));
+  const file1 = parse(readFile([filepath1]), getExt(filepath1));
+  const file2 = parse(readFile([filepath2]), getExt(filepath2));
   const difference = genDiff(file1, file2);
   return formatDefault(difference);
 };

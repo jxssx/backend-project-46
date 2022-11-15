f3cec7b9823303aa49185bd72d270fbaed29e2da
jxssx <danil.druzhina@gmail.com>
2022-11-15 13:18:25 +0300

    change tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,9 @@ import diff from '../index.js';
 import { readFile } from '../src/tools.js';
 
 test('diff', () => {
-  const actual = diff('__fixtures__/file1.json', '__fixtures__/file2.json');
-  expect(actual).toEqual(readFile(['__fixtures__/expected.txt'], 'utf8'));
+  const actual1 = diff('__fixtures__/file1.json', '__fixtures__/file2.json');
+  expect(actual1).toEqual(readFile(['__fixtures__/expected.txt'], 'utf8'));
+
+  const actual2 = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml');
+  expect(actual2).toEqual(readFile(['__fixtures__/expected.txt'], 'utf8'));
 });

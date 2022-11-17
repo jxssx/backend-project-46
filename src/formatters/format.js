a43b04a45929c6dffbb78adad6bdd6e9bb114e66
jxssx <danil.druzhina@gmail.com>
2022-11-17 20:49:34 +0300

    rename format.js

--- src/formatters/format.js
+++
@@ -1,9 +0,0 @@
-import formatStylish from './stylish.js';
-
-export default (data, format) => {
-  switch (format) {
-    case 'stylish':
-      return formatStylish(data);
-    default: throw new Error(`Unexpected output format "${format}"`);
-  }
-};

348155eb653e17b075a92293d9ea0e2c21f0cce8
jxssx <danil.druzhina@gmail.com>
2022-11-19 17:45:04 +0300

    change tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,9 +3,13 @@ import diff from '../index.js';
 import { readFile } from '../src/tools.js';
 
 test('diff', () => {
-  const actual1 = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish');
-  expect(actual1).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
+  const stylishJSON = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish');
+  const stylishYAML = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'stylish');
+  expect(stylishJSON).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
+  expect(stylishYAML).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
 
-  const actual2 = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'stylish');
-  expect(actual2).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
+  const plainJSON = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
+  const plainYAML = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'plain');
+  expect(plainJSON).toEqual(readFile(['__fixtures__/expectedPlain.txt'], 'utf8'));
+  expect(plainYAML).toEqual(readFile(['__fixtures__/expectedPlain.txt'], 'utf8'));
 });

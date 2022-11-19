49ff011b7e90dff88935f9535fd4346f9822515e
jxssx <danil.druzhina@gmail.com>
2022-11-19 18:03:32 +0300

    update formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,17 @@
 import formatStylish from './stylish.js';
 import formatPlain from './plain.js';
 
+const formatJSON = (data) => JSON.stringify(data, null, 2);
+
 export default (data, format) => {
   switch (format) {
     case 'stylish':
       return formatStylish(data);
     case 'plain':
       return formatPlain(data);
+    case 'JSON':
+    case 'json':
+      return formatJSON(data);
     default: throw new Error(`Unexpected output format "${format}"`);
   }
 };

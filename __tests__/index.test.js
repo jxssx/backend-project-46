b7003ffff98f949052008f243e3f605ef5da92e6
jxssx <danil.druzhina@gmail.com>
2022-11-19 18:03:59 +0300

    change tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,13 +3,18 @@ import diff from '../index.js';
 import { readFile } from '../src/tools.js';
 
 test('diff', () => {
-  const stylishJSON = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish');
-  const stylishYAML = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'stylish');
-  expect(stylishJSON).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
-  expect(stylishYAML).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
+  const stylishInJSON = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish');
+  const stylishInYAML = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'stylish');
+  expect(stylishInJSON).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
+  expect(stylishInYAML).toEqual(readFile(['__fixtures__/expectedStylish.txt'], 'utf8'));
 
-  const plainJSON = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
-  const plainYAML = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'plain');
-  expect(plainJSON).toEqual(readFile(['__fixtures__/expectedPlain.txt'], 'utf8'));
-  expect(plainYAML).toEqual(readFile(['__fixtures__/expectedPlain.txt'], 'utf8'));
+  const plainInJSON = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
+  const plainInYAML = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'plain');
+  expect(plainInJSON).toEqual(readFile(['__fixtures__/expectedPlain.txt'], 'utf8'));
+  expect(plainInYAML).toEqual(readFile(['__fixtures__/expectedPlain.txt'], 'utf8'));
+
+  const jsonInJSON = diff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json');
+  const jsonInYAML = diff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'json');
+  expect(jsonInJSON).toEqual(readFile(['__fixtures__/expectedJSON.txt'], 'utf8'));
+  expect(jsonInYAML).toEqual(readFile(['__fixtures__/expectedJSON.txt'], 'utf8'));
 });
